2b56ef6e1cac4a97083bab98107e541e0c32b1d5
sammorrisdesign <mail@sammorrisdesign.co.uk>
2016-08-04 18:01:26 -0400

    initial framework

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# podcards
+Guardian Podcards
+========================
+
+A way to autogenerate assets relating to our podcasts
+
+## Installation
+
+Get dependencies by running `npm install`. 
+
+Then you'll need add your s3 keys. Duplicate/rename the `aws-keys.example.json` file to `aws-keys.json` and add your s3 keys.
+
+## Usage
+
---
+++ scripts/compile.js
@@ -0,0 +1,29 @@
+// requirements
+var handlebars = require('handlebars');
+var fs = require('fs');
+var mkpath = require('mkpath');
+
+// templates
+var thrasherHtml = fs.readFileSync('src/templates/thrasher.html', 'utf8');
+var thrasherJson = fs.readFileSync('src/templates/thrasher.json', 'utf8');
+
+// data
+var podcasts = require('../src/podcasts.json');
+
+for (var i = 0; i < podcasts.length; i++) {
+    generateThrasherCode(podcasts[i]);
+}
+
+function generateThrasherCode(data) {
+    var htmlTemplate = handlebars.compile(thrasherHtml);
+        data.html = JSON.stringify(htmlTemplate(data), null);
+
+    var jsonTemplate = handlebars.compile(thrasherJson);
+    var json = jsonTemplate(data);
+
+    var path = 'build/thrashers/' + data.title;
+    mkpath(path, function() {
+        fs.writeFile(path + '/source.json', json);
+    });
+}
+
