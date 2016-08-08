02282522115aed850af10373bdbb1b8f536991e8
sammorrisdesign <mail@sammorrisdesign.co.uk>
2016-08-08 18:50:39 -0400

    add an illustration, do some more compile work, make thrasher look normal on desktop and above

--- scripts/compile.js
+++ scripts/compile.js
@@ -2,6 +2,7 @@
 var handlebars = require('handlebars');
 var fs = require('fs');
 var mkpath = require('mkpath');
+var ncp = require('ncp').ncp;
 
 // templates
 var thrasherHtml = fs.readFileSync('src/templates/thrasher.html', 'utf8');
@@ -13,11 +14,12 @@ var podcasts = require('../src/podcasts.json');
 
 for (var i = 0; i < podcasts.length; i++) {
     generateThrasherCode(podcasts[i]);
+    copyIllustrations();
 }
 
 function generateThrasherCode(data) {
     var htmlTemplate = handlebars.compile(thrasherHtml);
-        data.css = thrasherCss;
+        data.css = thrasherCss.replace(/handle/g, data.handle);
         data.html = JSON.stringify(htmlTemplate(data), null);
 
     var jsonTemplate = handlebars.compile(thrasherJson);
@@ -27,5 +29,14 @@ function generateThrasherCode(data) {
     mkpath(path, function() {
         fs.writeFile(path + '/source.json', json);
     });
+}
+
+function copyIllustrations() {
+    ncp('src/illustrations', 'build/illustrations', function(err) {
+        if(err) {
+            console.log(err);
+        }
+        console.log("Illustrations copied");
+    }); 
 }
 
