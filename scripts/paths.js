9ff798b114042234c44b922451d5f89c4610b977
sammorrisdesign <mail@sammorrisdesign.co.uk>
2016-08-11 16:12:47 -0400

    create config and generate paths from that

--- scripts/paths.js
+++ scripts/paths.js
@@ -4,6 +4,7 @@ var colors = require('colors');
 
 // data
 var podcasts = require('../src/podcasts.json');
+var config = require('../scripts/config.json');
 
 var arrayOfPodcasts = [];
 
@@ -14,13 +15,13 @@ for (var i = 0; i < podcasts.length; i++) {
 var selectedPodcast = readlineSync.keyInSelect(arrayOfPodcasts, 'What podcast do ya wanna know about?');
 
 console.log('Thrashers'.rainbow.bold.underline.inverse);
-console.log('Local Url '.bold.yellow + returnPath('http://localhost:8080/thrashers/' + podcasts[selectedPodcast].handle + '/source.json'));
-console.log('Remote Url '.bold.green + returnPath('https://interactive.guim.co.uk/podcasts/thrashers/' + podcasts[selectedPodcast].handle + '/source.json'));
+console.log('Local Url '.bold.yellow + returnPath('local', 'thrashers'));
+console.log('Remote Url '.bold.green + returnPath('remote', 'thrashers'));
 
 console.log('\nPodcard'.rainbow.bold.underline.inverse);
-console.log('Local Url '.bold.yellow + returnPath('http://localhost:8080/snap/' + podcasts[selectedPodcast].handle + '/source.json'));
-console.log('Remote Url '.bold.green + returnPath('https://interactive.guim.co.uk/podcasts/snap/' + podcasts[selectedPodcast].handle + '/source.json'));
+console.log('Local Url '.bold.yellow + returnPath('local', 'snap'));
+console.log('Remote Url '.bold.green + returnPath('remote', 'snap'));
 
-function returnPath(location) {
-    return podcasts[selectedPodcast].url + '?gu-snapType=json.html&gu-snapUri=' + encodeURIComponent(location) + '&gu-headline=' + encodeURIComponent(podcasts[selectedPodcast].title) + '&gu-trailText=' + encodeURIComponent(podcasts[selectedPodcast].description);
+function returnPath(env, type) {
+    return podcasts[selectedPodcast].url + '?gu-snapType=json.html&gu-snapUri=' + encodeURIComponent(config[env].url + (env == 'local' ? ":" + config.local.port : "") + "/" + type + "/" + podcasts[selectedPodcast].handle + '/source.json') + '&gu-headline=' + encodeURIComponent(podcasts[selectedPodcast].title) + '&gu-trailText=' + encodeURIComponent(podcasts[selectedPodcast].description);
 }
