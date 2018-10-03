dc071eac1c826a08ea2d1efea3c763326633ad8f
Anna Leach <anna,leach@theguardian.com>
2018-10-03 13:23:48 +0100

    Set up thrasher 2 template

--- scripts/compile.js
+++ scripts/compile.js
@@ -5,13 +5,17 @@ var mkpath = require('mkpath');
 var ncp = require('ncp').ncp;
 
 // templates
+var thrasherHtml1 = fs.readFileSync('src/templates/thrasher-1.html', 'utf8');
+var thrasherCss1 = fs.readFileSync('build/css/thrasher-1.css', 'utf8');
+
+var thrasherHtml2 = fs.readFileSync('src/templates/thrasher-2.html', 'utf8');
+var thrasherCss2 = fs.readFileSync('build/css/thrasher-2.css', 'utf8');
+
 var genericJson = fs.readFileSync('src/templates/html.json', 'utf8');
-var thrasherHtml = fs.readFileSync('src/templates/thrasher.html', 'utf8');
-var thrasherCss = fs.readFileSync('build/css/thrasher.css', 'utf8');
 var snapHtml = fs.readFileSync('src/templates/snap.html', 'utf8');
 var snapCss = fs.readFileSync('build/css/snap.css', 'utf8');
 var homeHtml = fs.readFileSync('src/templates/index.html', 'utf8');
-var fakeFrontend = fs.readFileSync('src/templates/fakeFrontend.html', 'utf8');
+var fakeFrontend = fs.readFileSync('src/templates/fake-frontend.html', 'utf8');
 
 // data
 var podcasts = require('../src/podcasts.json');
@@ -26,7 +30,7 @@ generateHomePage();
 copyIllustrations();
 
 
-function generateJson(data, css, template){
+function generateJson(data, css, template) {
   var htmlTemplate = handlebars.compile(template);
     data.css = css.replace(/handle/g, data.handle);
     data.html = JSON.stringify(htmlTemplate(data), null);
@@ -36,11 +40,23 @@ function generateJson(data, css, template){
 }
 
 function generateThrasher(data) {
-    var thrasherJson = generateJson(data, thrasherCss, thrasherHtml);
-    var path = 'build/thrasher/' + data.handle;
-    mkpath(path, function() {
-        fs.writeFile(path + '/source.json', thrasherJson);
-    });
+    var designVersion = parseInt(data.designVersion);
+
+    if(designVersion === 1){
+      var thrasherJson1 = generateJson(data, thrasherCss1, thrasherHtml1);
+      writeThrasher(data.handle, thrasherJson1)
+    }
+    else if (designVersion === 2){
+      var thrasherJson2 = generateJson(data, thrasherCss2, thrasherHtml2);
+      writeThrasher(data.handle, thrasherJson2)
+    }
+}
+
+function writeThrasher (handle, json) {
+  var path = 'build/thrasher/' + handle;
+  mkpath(path, function() {
+    fs.writeFile(path + '/source.json', json);
+  });
 }
 
 function generateSnap(data) {
@@ -53,9 +69,22 @@ function generateSnap(data) {
 
 
 // preview for viewing thrashers on localhost
-function generatePreview(data){
-  var thrasherTemplate = handlebars.compile(thrasherHtml);
-  data.css = thrasherCss.replace(/handle/g, data.handle);
+function generatePreview(data) {
+  if(data.designVersion === 1) {
+    var html = thrasherHtml1;
+    var css = thrasherCss1;
+  } else {
+    var html = thrasherHtml2;
+    var css = thrasherCss2;
+  }
+
+  handlebars.registerHelper('makeTitle', function(str) {
+    var caps = str.charAt(0).toUpperCase() + str.slice(1);
+    return caps.replace("-", " ");
+  });
+
+  var thrasherTemplate = handlebars.compile(html);
+  data.css = css.replace(/handle/g, data.handle);
   handlebars.registerPartial('thrasherPartial', thrasherTemplate(data));
 
   var fakeFrontendTemplate = handlebars.compile(fakeFrontend);
